de75398b7c56ce1e8b05c149510d38b6762ae388
SandanuwanJS <jayathuthalagala@gmail.com>
2024-11-21 14:51:08 +0530

    disctrict list changes

--- src/components/theater/Districts.jsx
+++ src/components/theater/Districts.jsx
@@ -1,30 +1,33 @@
 import React from "react";
-import DistrictList from "../../data/DistrictList.json"
+import DistrictList from "../../data/DistrictList.json";
 import { useNavigate } from "react-router";
 
 export default function Districts() {
   const navigate = useNavigate();
 
-  function theaterlistButton () {
-    navigate ("/theaterlist");
-
+  function theaterlistButton(id) {
+    alert(`District ID: ${id}`);
+    // You can navigate if needed, e.g., navigate(`/theaterlist/${id}`);
   }
+
   return (
-    <div className="mt-[120px] p-4">
-      <h1 className="text-3xl font-semibold mb-6 text-center">District List</h1>
-	  <h1 classname="text-3xl font-bold mb-6 text-center"> Click District name to view Theaters List</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+    <div className="mt-[70px] p-4 font-redHat">
+      <h1 className="text-3xl font-extrabold mb-6 text-center">District List</h1>
+      <div className="mt-[2px]">
+        <p className="text-2xl font-semibold mb-6 text-center">
+          Click District name to view Theaters List and Details
+        </p>
+      </div>
+      <div className="mt-[20px] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
         {DistrictList.map((district, index) => (
           <button
-
-          
             key={index}
+            onClick={() => theaterlistButton(district.id)}
             className="bg-[#004A8F] text-gray-300 font-bold py-3 px-5 rounded-lg shadow-md hover:bg-[#FCB81F] hover:text-black transition duration-300 ease-in-out transform hover:scale-105"
           >
-            <div onClick={theaterlistButton} className="text-center">
+            <div className="text-center">
               <p className="font-bold">{district.english}</p>
-              <p className="text-sm ">{district.sinhala}</p>
-              <p classname="text-bold">{district.id}</p>
+              <p className="text-sm">{district.sinhala}</p>
             </div>
           </button>
         ))}
